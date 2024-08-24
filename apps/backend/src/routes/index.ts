b88c03b6d3b5619a5628c929aae4ecfd1d90802a
Turbobot <turbobot@vercel.com>
2024-08-24 23:40:19 +0530

    feat(create-turbo): apply official-starter transform

--- apps/backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import userAuth from "./users";
-
-const router = express.Router();
-
-router.use("/user", userAuth);
-
-export default router;
--- apps/backend/src/routes/users.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from "express";
-
-const router = express.Router();
-
-router.post("/signup", async (req: Request, res: Response) => {
-  const { username, phonenumber, email, password } = req.body;
-});
-
-export default router;
