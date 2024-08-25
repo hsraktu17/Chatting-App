894db9128cd3de10a5d3e66c45335186b1892f4e
Utkarsh <78083867+hsraktu17@users.noreply.github.com>
2024-08-25 13:55:16 +0530

    prisma reconfiured

---
+++ apps/ws/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const PORT = 8080;
+
+const server = http.createServer(app);
+const io = new Server(server);
+const hello = "hell";
+
+const users: { [key: string]: string } = {};
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("join", (username) => {
+    users[socket.id] = username;
+    console.log(`${username} joined with ID: ${socket.id}`);
+    console.log(users);
+  });
+
+  socket.on("private", ({ receiver, message }) => {
+    const senderUsername = users[socket.id];
+    const receiverName = users[receiver];
+    if (senderUsername && receiver && message) {
+      io.to(receiver).emit("private", {
+        receiver: receiverName,
+        sender: senderUsername,
+        message,
+      });
+    } else {
+      console.log("Error sending message: Invalid data");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+    delete users[socket.id];
+  });
+});
+
+server.listen(PORT, () =>
+  console.log(`WebSocket server is running on port ${PORT}`),
+);
